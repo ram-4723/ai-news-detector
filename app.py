c4ed65bcd0119db9f97b6064e9958d14c988bab4
Ramachandran Yadav <ramy2212024@gmail.com>
2026-05-10 19:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,8 @@ def analyze():
         "ocr_text": extracted_text
     })
 
+    import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
